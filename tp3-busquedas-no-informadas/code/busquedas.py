ee9c66795710e497a9db49b3182b275a9922107e
unknown <sorbello.mauro.em.cn@gmail.com>
2024-09-04 11:01:24 -0300

    dfs & dls

--- tp3-busquedas-no-informadas/code/busquedas.py
+++ tp3-busquedas-no-informadas/code/busquedas.py
@@ -70,8 +70,113 @@ def bfs(env):
         path.reverse()  
         return solution, path
 
+def dfs(env):
+    desc = env.unwrapped.desc
+    desc_str = [[cell.decode('utf-8') for cell in row] for row in desc]
+    print(desc_str)
+    # Crear la cola FIFO de frontera usando deque
+    frontier = []
+    explored = set()
+    len_desc = len(desc)
+    parent = {}
+    # Busco el S: Start
+    for i in range (len_desc):
+        for j in range (len_desc):
+            if desc_str[i][j] == "S":
+                frontier.append((i,j))
+                parent[(i,j)] = None
+    check = False
+    solution = None
+    while check == False:
+        if len(frontier) == 0:
+            break
+        print(frontier)
+        node = frontier.pop()
+        print(frontier)
+        explored.add(node)
+        # Devuelve las posibles acciones
+        actions = choose_action(node[0], node[1], len_desc)
+        for act in actions:
+            child_node = action(act, node)
+            if desc_str[child_node[0]][child_node[1]] != "H":
+                if (child_node not in explored) and (child_node not in frontier):
+                    parent[child_node]=node
+                    if desc_str[child_node[0]][child_node[1]] == "G":
+                        print(child_node)
+                        solution = child_node
+                        check = True
+                        break
+                    else:
+                        frontier.append(child_node)
+    if solution == None:
+        return False
+    else:
+        path = []
+        sol = solution
+        while sol is not None:
+            if sol == None:
+                break
+            path.append(sol)
+            sol = parent[sol]
+        path.reverse()  
+        return solution, path
+
+# depth limited search
+def dls(env,limit): 
+    desc = env.unwrapped.desc
+    desc_str = [[cell.decode('utf-8') for cell in row] for row in desc]
+    print(desc_str)
+    # Crear la cola FIFO de frontera usando deque
+    frontier = []
+    explored = set()
+    len_desc = len(desc)
+    parent_profundidad = {}
+    # Busco el S: Start
+    for i in range (len_desc):
+        for j in range (len_desc):
+            if desc_str[i][j] == "S":
+                frontier.append((i,j))
+                parent_profundidad[(i,j)] = (None,1)
+    check = False
+    solution = None
+    while check == False:
+        if len(frontier) == 0:
+            break
+        print(frontier)
+        node = frontier.pop()
+        print(frontier)
+        explored.add(node)
+        # Devuelve las posibles acciones
+        actions = choose_action(node[0], node[1], len_desc)
+        for act in actions:
+            child_node = action(act, node)
+            if desc_str[child_node[0]][child_node[1]] != "H":
+                if (child_node not in explored) and (child_node not in frontier):
+                    if (parent_profundidad[node][1] + 1) <= limit:
+                        parent_profundidad[child_node]=(node,parent_profundidad[node][1] + 1)
+                        if desc_str[child_node[0]][child_node[1]] == "G":
+                            print(child_node)
+                            solution = child_node
+                            check = True
+                            break
+                        else:
+                            frontier.append(child_node)
+    if solution == None:
+        return False
+    else:
+        path = []
+        sol = solution
+        while sol is not None:
+            if sol == None:
+                break
+            path.append(sol)
+            sol = parent_profundidad[sol][0]
+        path.reverse()  
+        return solution, path
+    
 env, desc = generate_random_map_custom.generate_random_map_custom(4, 0.4, 3)
 print(desc[2][0])
 
-recorrido = bfs(env)
+recorrido = dls(env,2)
 print(recorrido)
+
